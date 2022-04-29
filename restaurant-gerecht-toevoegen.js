9bdae6156cf05ecd5b7792720d77d4d226f567bc
Yi Fong Au <yifongau@gmail.com>
2022-04-29 18:06:05 +0200

    resto admin toont lijst met gerechten

--- restaurant-gerecht-toevoegen.js
+++ restaurant-gerecht-toevoegen.js
@@ -1,48 +1,47 @@
-// Haalt naam restaurant op op basis van query
-// const queryString = window.location.search;
-// const urlParams = new URLSearchParams(queryString);
-// const restaurantid = urlParams.get("id");
-// fetch("http://localhost:8080/restaurantbyid/" + restaurantid)
-//     .then((Response) => Response.json())
-//     .then((data) => {
-//     var restaurantnaam = data.naam;
-//     document.getElementById("restaurantnaam").innerHTML +=
-//         "Menukaart " + restaurantnaam;
-//     });
 
-fetch("./data/restaurants.json")
-.then((response) => response.json())
-.then((restaurants) => {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const restaurantid = parseInt(urlParams.get("id"), 10);
-    const restaurant = restaurants.find(restaurants => restaurants.id === restaurantid);
+// haalt restaurantid op uit url.
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const restaurantid = parseInt(urlParams.get("id"), 10);
 
-    var restaurantnaam = restaurant.naam;
-    const h1 = document.getElementById("restaurantnaam");
-    let h1String = "Adminpagina: ";
-    h1String += restaurantnaam;
-    h1.innerHTML = h1String;
-
-
-    const listEL = document.getElementById("gerechten");
-    let htmlString = "";
-    restaurant.gerechten.forEach((gerecht) => {
-        htmlString += template_gerecht(gerecht);
+// hier wordt de restaurantnaam opgehaald en aan bezNav gegeven.
+fetch("https://backendyc2204bezorging.azurewebsites.net/restaurantbyid/" + restaurantid)
+    .then((response) => response.json())
+    .then((restaurantdata) => {
+        let restaurantnaam = restaurantdata.naam;
+        const bezNav = document.querySelector("bez-nav");
+        bezNav.title = restaurantnaam + " - Adminpagina - Gerechten toevoegen";
     })
-    listEL.innerHTML = htmlString;
-})
 
+// hier worden de restaurantgerechten opgehaald en in lijstitems gestopt.
+fetch("https://backendyc2204bezorging.azurewebsites.net/toonmenu/" + restaurantid)
+    .then((response) => response.json())
+    .then((gerechtendata) => {
+        const listEL = document.getElementById("gerechten");
+        let htmlString = "";
+        gerechtendata.forEach((gerecht) => {
+            htmlString += template_gerecht(gerecht);
+        })
+        listEL.innerHTML = htmlString;
 
+    })
+
+// template voor lijstitems van gerechten
 function template_gerecht(gerecht) {
     return `
     <li class="flex-item">
-    <div>
-    <p>${gerecht.naam}</p>
-    <p>Prijs €: ${gerecht.prijs}</p>
-    </div>
+        <div>
+            <p>${gerecht.naam}</p>
+            <p>Prijs: € ${gerecht.prijs}</p>
+        </div>
 
-    <img src=${gerecht.afbeelding}>
-    <button onclick=rmgerecht()>Verwijder</button>
+        <div>
+            <img src=${gerecht.afbeelding}>
+        </div> 
+        <button>
+            Verwijderen
+        </button> 
+
     </li>`
 }
+
