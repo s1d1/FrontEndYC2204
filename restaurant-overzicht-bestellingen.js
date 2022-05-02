a4eb7bf18317519319772e117f3420a82c108377
Yi Fong Au <yifongau@gmail.com>
2022-05-02 15:40:27 +0200

    deel van data in lijst gekregen

--- restaurant-overzicht-bestellingen.js
+++ restaurant-overzicht-bestellingen.js
@@ -6,17 +6,45 @@ const urlParams = new URLSearchParams(queryString);
 const restaurantid = parseInt(urlParams.get("id"), 10);
 
 // template voor lijstitems van bestelling
-function template_bestelling (bestelling) {
+function template_bestelling(bestelling) {
     return `
     <li class="flex-item">
         <div>
-            <p>Naam: ${bestelling.klant.naam}</p>
+            <h3>Bestelgegevens</h3>
+            <p>#${bestelling.id}</p>
+            <p>Tijdstip: ${bestelling.tijdstip}</p>
+            <p>Status:${bestelling.status}</p>
+            <p>Betaald: ${bestelling.betaald}</p>
+        </div>
+        <div>
+
+            <h3>Klantgegevens</h3>
+            <p>Naam: ${bestelling.klant.voornaam} ${bestelling.klant.achternaam}</p>
+            <p>Afleveradres: ${bestelling.klant.adress}, ${bestelling.klant.postcode}</p>
+            <p>Telefoonnummer:</p>
+            <p>email: ${bestelling.klant.email}</p>
+
+
+        </div>
+        <div>
+
+            <h3>Gerechten</h3>
+            <p>Naam: ${bestelling.gerechten}</p>
+       
+
+
         </div>
 
-       
+
         <button>
-            Status veranderen
-        </button> 
+            Bestelling annuleren
+        </button>
+        <button>
+            Bestelling bereid
+        </button>
+        <button>
+            Bezorger toewijzen
+        </button>
 
     </li>`
 
