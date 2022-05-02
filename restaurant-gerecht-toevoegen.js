571face4b8d2614853293c5358789ad2995a8c99
Yi Fong Au <yifongau@gmail.com>
2022-05-02 10:50:54 +0200

    admin toont nu restaurants uit azure database

--- restaurant-gerecht-toevoegen.js
+++ restaurant-gerecht-toevoegen.js
@@ -26,51 +26,52 @@ function template_gerecht(gerecht) {
 
 }
 
-//fetch functie met lokale DUMMY data.
-fetch("./data/restaurants.json")
-    .then((response) => response.json())
-    .then((restaurantdata) => {
-        const restaurant = restaurantdata.find(restaurant => restaurant.id === restaurantid);
+// fetch functie met lokale DUMMY data.
+// fetch("./data/restaurants.json")
+//     .then((response) => response.json())
+//     .then((restaurantdata) => {
+//         const restaurant = restaurantdata.find(restaurant => restaurant.id === restaurantid);
 
-        var restaurantnaam = restaurant.naam;
-        const bezNav = document.querySelector("bez-nav");
-        console.log(restaurantnaam, bezNav);
-        bezNav.title = restaurantnaam + " - Adminpagina - Gerechten toevoegen";
-
-
-        const listEL = document.getElementById("gerechten");
-        let htmlString = "";
-        restaurant.gerechten.forEach((gerecht) => {
-            htmlString += template_gerecht(gerecht);
-            console.log(gerecht);
-        })
+//         var restaurantnaam = restaurant.naam;
+//         const bezNav = document.querySelector("bez-nav");
+//         console.log(restaurantnaam, bezNav);
+//         bezNav.title = restaurantnaam + " - Adminpagina - Gerechten toevoegen";
 
 
+//         const listEL = document.getElementById("gerechten");
+//         let htmlString = "";
+//         restaurant.gerechten.forEach((gerecht) => {
+//             htmlString += template_gerecht(gerecht);
+//             console.log(gerecht);
+//         })
+
+//         listEL.innerHTML = htmlString;
+//     })
 
 
         // hier wordt de restaurantnaam opgehaald uit DATABASE en aan bezNav gegeven.
-        // fetch("https://backendyc2204bezorging.azurewebsites.net/restaurantbyid/" + restaurantid)
-        //     //fetch("https://localhost:8080/restaurantbyid/" + restaurantid)
-        //     .then((response) => response.json())
-        //     .then((restaurantdata) => {
-        //         let restaurantnaam = restaurantdata.naam;
-        //         const bezNav = document.querySelector("bez-nav");
-        //         bezNav.title = restaurantnaam + " - Adminpagina - Gerechten toevoegen";
-        //     })
+        fetch("https://backendyc2204bezorging.azurewebsites.net/restaurantbyid/" + restaurantid)
+            //fetch("https://localhost:8080/restaurantbyid/" + restaurantid)
+            .then((response) => response.json())
+            .then((restaurant) => {
+                let restaurantnaam = restaurant.naam;
+                const bezNav = document.querySelector("bez-nav");
+                bezNav.title = restaurantnaam + " - Adminpagina - Gerechten toevoegen";
+            })
+            
 
-        // hier worden de restaurantgerechten opgehaald uit DATABASE en in lijstitems gestopt.
-        // fetch("https://backendyc2204bezorging.azurewebsites.net/toonmenu/" + restaurantid)
-        //     //fetch("https://localhost:8080/toonmenu/" + restaurantid)
-        //     .then((response) => response.json())
-        //     .then((gerechtendata) => {
-        //         const listEL = document.getElementById("gerechten");
-        //         let htmlString = "";
-        //         gerechtendata.forEach((gerecht) => {
-        //             htmlString += template_gerecht(gerecht);
-        //         })
-
-        listEL.innerHTML = htmlString;
-    })
+     //   hier worden de restaurantgerechten opgehaald uit DATABASE en in lijstitems gestopt.
+        fetch("https://backendyc2204bezorging.azurewebsites.net/toonmenu/" + restaurantid)
+            //fetch("https://localhost:8080/toonmenu/" + restaurantid)
+            .then((response) => response.json())
+            .then((gerechtendata) => {
+                const listEL = document.getElementById("gerechten");
+                let htmlString = "";
+                gerechtendata.forEach((gerecht) => {
+                    htmlString += template_gerecht(gerecht);
+                })
+                listEL.innerHTML = htmlString;
+                   })
 
 
 
