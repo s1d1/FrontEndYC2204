5e8c7701be42a5979e5dfe6006a57adb9625a14c
yifongau <yifongau@gmail.com>
2022-05-05 13:53:46 +0200

    werkende navbar met css + resto overzicht bestellingen rudimentair

--- components/w3-navbar.js
+++ components/w3-navbar.js
@@ -28,6 +28,14 @@ class w3NavBar extends HTMLElement {
   
     render() {
       this.shadowRoot.innerHTML = `
+      <link rel="stylesheet" href="w3-template.css">
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Atkinson+Hyperlegible:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
       <div class="w3-top">
       <div class="flex-row w3-bar w3-xlarge w3-black w3-grayscale-max w3-opacity w3-hover-opacity-off" id="myNavbar">
          
