03f13f5f2f795e66d1a4439bf7559fb6d0390152
Yi Fong Au <yifongau@gmail.com>
2022-05-12 11:00:21 +0200

    start bouwen functie om tijd op te halen

---
+++ components/localdatetime.js
@@ -0,0 +1,12 @@
+window.watIsDeTijd = function() {
+
+    let hDatum = new Date();
+    let hDag = hDatum.getDate();
+    let hMaand = hDatum.getMonth();
+    let hJaar = hDatum.getYear();
+
+    console.log(hDag);
+    console.log(hMaand);
+    console.log(hJaar);
+
+}
